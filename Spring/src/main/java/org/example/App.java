247009c3cfe12c78930f594e83326c477c8de66f
skyclouds2001 <skyclouds2001@163.com>
2022-11-05 20:40:25 +0800

    feat: learn spring

--- Spring/src/main/java/org/example/App.java
+++ Spring/src/main/java/org/example/App.java
@@ -4,6 +4,8 @@ import org.example.dao.BookDao;
 import org.example.service.BookService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+
 public class App {
 
     public static void main(String[] args) {
@@ -16,6 +18,14 @@ public class App {
 
         BookDao bookDao = (BookDao) ctx.getBean("bookDaoFactoryBean");
         bookDao.save();
+
+        /* ***** Example ***** */
+
+        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+        System.out.println(dataSource);
+
+        DataSource dataSource1 = (DataSource) ctx.getBean("dataSourceD");
+        System.out.println(dataSource1);
 
         ctx.close();
     }
