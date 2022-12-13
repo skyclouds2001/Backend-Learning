c27238efc7cb2c9877b46eb12f93cafff77fd5f4
skyclouds2001 <skyclouds2001@163.com>
2022-12-13 14:25:18 +0800

    feat: learn SSM - add customized exception & handler

--- SSM/src/main/java/org/sky/controller/ProjectExceptionHandler.java
+++ SSM/src/main/java/org/sky/controller/ProjectExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.sky.controller;
 
+import org.sky.exception.BusinessException;
+import org.sky.exception.SystemException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,9 +9,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ProjectExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public void solveException(Exception e) {
-        System.out.println(1);
-        e.printStackTrace();
+    public RequestResult solveException(Exception e) {
+        return new RequestResult(RequestCode.FAILURE, e.getMessage(), false, null);
+    }
+
+    @ExceptionHandler(SystemException.class)
+    public RequestResult solveSystemException(SystemException e) {
+        return new RequestResult(e.getCode(), e.getMessage(), false, null);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public RequestResult solveBusinessException(BusinessException e) {
+        return new RequestResult(e.getCode(), e.getMessage(), false, null);
     }
 
 
