d0a4683540fad6f2613361b802cde8804c77ee62
skyclouds2001 <skyclouds2001@163.com>
2022-12-13 18:16:36 +0800

    feat: learn SSM - add Interceptor

--- SSM/src/main/java/org/sky/config/SpringMvcConfig.java
+++ SSM/src/main/java/org/sky/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("org.sky.controller")
+@ComponentScan({"org.sky.controller", "org.sky.config"})
 @EnableWebMvc
 public class SpringMvcConfig {
 }
--- SSM/src/main/java/org/sky/config/SpringMvcSupport.java
+++ SSM/src/main/java/org/sky/config/SpringMvcSupport.java
@@ -1,11 +1,17 @@
 package org.sky.config;
 
+import org.sky.controller.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class SpringMvcSupport extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -13,6 +19,11 @@ public class SpringMvcSupport extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/css/**").addResourceLocations("/css");
         registry.addResourceHandler("/js/**").addResourceLocations("/js");
         registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins");
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/course", "/course/*");
     }
 
 }
