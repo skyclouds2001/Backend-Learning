4035afd7c7bbed65df81123ccb0b7d70ea52d00c
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-07-27 16:30:56 +0800

    Update var.go

--- Go/var.go
+++ Go/var.go
@@ -1,5 +1,7 @@
 package main
 
+import "strconv"
+
 var (
 	num int
 	str string
@@ -27,4 +29,17 @@ func main() {
 	bb, nn, cc, ss := b, n, c, s
 
 	println(bb, nn, cc, ss)
+
+	var n1 int64 = 10
+	var n2 = float64(n1)
+	println(n1, n2)
+
+	println(strconv.Atoi("1"))
+	println(strconv.Itoa(1))
+	println(strconv.ParseFloat("3.14", 64))
+	println(strconv.FormatFloat(3.1415926, 'f', 2, 64))
+
+	var i interface{} = "Hello, World"
+	str, ok := i.(string)
+	println(str, ok)
 }
