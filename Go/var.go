95d4f85273427ed3b2fb9eaabb6549a3a2771359
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-07-27 11:42:57 +0800

    Create var.go

---
+++ Go/var.go
@@ -0,0 +1,30 @@
+package main
+
+var (
+	num int
+	str string
+)
+
+func main() {
+	println(str, num)
+
+	var b bool
+
+	println(b)
+
+	const n = 3
+
+	println(n)
+
+	var c = 'c'
+
+	println(c)
+
+	var s = "ssss"
+
+	println(s)
+
+	bb, nn, cc, ss := b, n, c, s
+
+	println(bb, nn, cc, ss)
+}
