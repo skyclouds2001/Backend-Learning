fd4fb18c9839dca489b005837e76c9cc6c72b9ca
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-07-27 12:11:01 +0800

    Update var.go

--- Go/var.go
+++ Go/var.go
@@ -27,4 +27,14 @@ func main() {
 	bb, nn, cc, ss := b, n, c, s
 
 	println(bb, nn, cc, ss)
+
+	var arr = [5]int{1, 2, 3, 4, 5}
+	var a = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	println(arr)
+	println(a)
+
+	var p *int
+	p = nil
+	println(p)
 }
