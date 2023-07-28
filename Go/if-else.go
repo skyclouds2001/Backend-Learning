8af7e64225179d42390b2a9797aaf01c218a44f2
skyclouds2001 <skyclouds2001@163.com>
2023-07-29 02:14:28 +0800

    feat: add if-else go

---
+++ Go/if-else.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	if 7 % 2 == 0 {
+		println("7 is even")
+	} else {
+		println("7 is odd")
+	}
+
+	if num:= 9; num < 0 {
+		println(num, "is negative")
+	} else if num < 10 {
+		println(num, "has 1 digit")
+	} else {
+		println(num, " has multiple digits")
+	}
+}
