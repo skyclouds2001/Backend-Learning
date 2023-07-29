13087cdc545076704ab29f67d90472e03ddb9853
skyclouds2001 <skyclouds2001@163.com>
2023-07-29 14:03:31 +0800

    feat: update print

--- Go/print.go
+++ Go/print.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+type Point struct {
+	x, y int
+}
+
 func main() {
 	fmt.Printf("code = %d %% %x %% %o %% %b & date = %s & value = %f \n", 123, 123, 123, 123, "2023-12-31", 12.3456789)
 
-	var str = fmt.Sprintf("code = %d %% %x %% %o %% %b & date = %s & value = %f \n", 123, 123, 123, 123, "2023-12-31", 12.3456789)
+	str := fmt.Sprintf("code = %d %% %x %% %o %% %b & date = %s & value = %f \n", 123, 123, 123, 123, "2023-12-31", 12.3456789)
 	fmt.Print(str)
+
+	point := Point{1, 2}
+	fmt.Printf("point=%v\n", point)
+	fmt.Printf("point=%+v\n", point)
+	fmt.Printf("point=%#v\n", point)
 }
