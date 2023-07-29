b046153c001ff3aa498a084eb318e0d58ab15ac4
skyclouds2001 <skyclouds2001@163.com>
2023-07-29 15:55:41 +0800

    feat: done os

---
+++ Go/os.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println(os.Args)
+
+	fmt.Println(os.Getenv("PATH"))
+	fmt.Println(os.Setenv("A", "a"))
+
+	buf, err := exec.Command("grep", "127..0.0.1", "/etc/hosts").CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(buf))
+}
