ef5befdd066fd1faa89fbc131977d3420fa931ab
skyclouds2001 <skyclouds2001@163.com>
2023-08-01 01:11:01 +0800

    feat: small updates

--- Go/concurrency.go
+++ Go/concurrency.go
@@ -14,9 +14,11 @@ func print(s string) {
 
 func sum(s []int, c chan int) {
 	sum := 0
+
 	for _, v := range s {
 		sum += v
 	}
+
 	c <- sum
 }
 
@@ -26,8 +28,11 @@ func main() {
 	print("c")
 
 	ch := make(chan int, 2)
+
 	go sum([]int{1, 2, 3}, ch)
 	go sum([]int{7, 8, 9}, ch)
+
 	x, y := <-ch, <-ch
+
 	println(x, y, x+y)
 }
