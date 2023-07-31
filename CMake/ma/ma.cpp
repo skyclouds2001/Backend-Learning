1f042780d608bd74880a48eb48eaade8d9763f46
skyclouds2001 <skyclouds2001@163.com>
2023-07-31 16:27:31 +0800

    feat: update cmake with generate library files

---
+++ CMake/ma/ma.cpp
@@ -0,0 +1,7 @@
+#ifdef USE_MA
+    #include "ma.h"
+
+    int ma::sqrt(int v) {
+        return v;
+    }
+#endif
---
+++ CMake/ma/ma.h
@@ -0,0 +1,8 @@
+#ifndef TUTORIAL_MA_H
+#define TUTORIAL_MA_H
+
+namespace ma {
+    int sqrt(int v);
+}
+
+#endif
