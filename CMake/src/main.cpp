4d2dde07ec29ccc2e95922e5e771b9d5a5b8541c
skyclouds2001 <skyclouds2001@163.com>
2023-07-31 15:48:31 +0800

    feat: update cmake with version control file and cxx version

--- CMake/src/main.cpp
+++ CMake/src/main.cpp
@@ -1,11 +1,19 @@
-//
-// Created by c50034865 on 2023/7/31.
-//
+/**
+ * @file main source file
+ * @author c50034865
+ * @date 2023/7/31
+ * @version 0.0
+ */
 
 #include "iostream"
+#include "TutorialConfig.h"
 
-int main() {
+int main(int argc, char const *argv[]) {
     std::cout << "Hello CMake!" << std::endl;
+
+    if (argc < 2) {
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+    }
 
     return 0;
 }
