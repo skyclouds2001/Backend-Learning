a1fe209fac8ebc2ffa7baf38dc98b59a121a471e
skyclouds2001 <skyclouds2001@163.com>
2023-08-02 22:06:41 +0800

    feat: update concurrency

--- Go/concurrency.go
+++ Go/concurrency.go
@@ -29,10 +29,33 @@ func main() {
 
 	ch := make(chan int, 2)
 
+	defer close(ch)
+
 	go sum([]int{1, 2, 3}, ch)
 	go sum([]int{7, 8, 9}, ch)
 
 	x, y := <-ch, <-ch
 
 	println(x, y, x+y)
+
+	src := make(chan int)
+	dest := make(chan int, 3)
+
+	go func ()  {
+		defer close(src)
+		for i := 0; i < 10; i++ {
+			src <- i
+		}
+	}()
+
+	go func() {
+		defer close(dest)
+		for i := range src {
+			dest <- i * i
+		}
+	}()
+
+	for i := range dest {
+		println(i)
+	}
 }
