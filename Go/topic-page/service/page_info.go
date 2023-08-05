767215491726bb8fa929595c29789a7b62ab72a6
skyclouds2001 <skyclouds2001@163.com>
2023-08-05 23:03:13 +0800

    feat: init basic controller and service

---
+++ Go/topic-page/service/page_info.go
@@ -0,0 +1,21 @@
+package service
+
+import "topic_page/repository"
+
+type PageInfo struct {
+	Topic    *repository.Topic
+	PostList []*repository.Post
+}
+
+func (f *QueryPageInfoFlow) Do() (*PageInfo, error) {
+	if err := f.checkParam(); err != nil {
+		return nil, err
+	}
+	if err := f.prepareInfo(); err != nil {
+		return nil, err
+	}
+	if err := f.packPageInfo(); err != nil {
+		return nil, err
+	}
+	return f.pageInfo, nil
+}
