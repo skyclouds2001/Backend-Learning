1ce53c296b702e34261df082afa4eaf057e08a1e
skyclouds2001 <skyclouds2001@163.com>
2023-08-05 21:54:13 +0800

    feat: add test view

---
+++ Go/demo_test.go
@@ -0,0 +1,19 @@
+package Go
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestDemo(t *testing.T) {
+	output := 1 + 1
+	expectOutput := 2
+	assert.Equal(t, output, expectOutput)
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	os.Exit(code)
+}
