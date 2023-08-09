d89b820d463929fd7ac08e8f75d579fd7b6773cd
skyclouds2001 <skyclouds2001@163.com>
2023-08-09 22:07:26 +0800

    feat: update post repository

--- Go/topic-page/repository/post.go
+++ Go/topic-page/repository/post.go
@@ -1,5 +1,12 @@
 package repository
 
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+	"sync"
+)
+
 var (
 	postIndexMap map[int64][]*Post
 )
@@ -9,4 +16,52 @@ type Post struct {
 	ParentId   int64  `json:"parent_id"`
 	Content    string `json:"content"`
 	CreateTime int64  `json:"create_time"`
+}
+
+type PostDao struct{}
+
+var (
+	postDao  *PostDao
+	postOnce sync.Once
+)
+
+func NewPostDaoInstance() *PostDao {
+	postOnce.Do(
+		func() {
+			postDao = &PostDao{}
+		})
+	return postDao
+}
+
+func (*PostDao) QueryPostById(id int64) []*Post {
+	return postIndexMap[id]
+}
+
+func InitPostIndexMap(filePath string) error {
+	open, err := os.Open(filePath + "post")
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(open)
+	postTmpMap := make(map[int64][]*Post)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		var post Post
+		if err := json.Unmarshal([]byte(text), &post); err != nil {
+			return err
+		}
+
+		posts, ok := postTmpMap[post.ParentId]
+		if !ok {
+			posts = make([]*Post, 0)
+		}
+		posts = append(posts, &post)
+		postTmpMap[post.ParentId] = posts
+	}
+
+	postIndexMap = postTmpMap
+
+	return nil
 }
