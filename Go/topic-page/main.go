1fe29964c2d9164868979ead31652c1ee7f36db1
skyclouds2001 <skyclouds2001@163.com>
2023-08-10 22:09:48 +0800

    feat: update main func

--- Go/topic-page/main.go
+++ Go/topic-page/main.go
@@ -2,10 +2,28 @@ package topic_page
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
+	"topic_page/controller"
+	"topic_page/repository"
 )
+
+func init() {
+	err := repository.Init()
+	if err != nil {
+		os.Exit(-1)
+	}
+}
 
 func main() {
 	r := gin.Default()
+
+	r.Use(gin.Logger())
+
+	r.GET("/community/page/:id", func(c *gin.Context) {
+		topicId := c.Param("id")
+		data := controller.QueryPageInfo(topicId)
+		c.JSON(200, data)
+	})
 
 	err := r.Run()
 	if err != nil {
